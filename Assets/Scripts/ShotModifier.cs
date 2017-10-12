4b407aaf6bb5a377af8ca48bab9ae634eea7dcd9
Unknown <gr1157@nyu.edu>
2017-10-12 13:29:03 -0400

    someWork

--- Assets/Scripts/ShotModifier.cs
+++ Assets/Scripts/ShotModifier.cs
@@ -6,13 +6,18 @@ public class ShotModifier : ScriptableObject {
 
 	public int maxUpgradeLevel = 0; //0 is infinity
 	public int currentLevel = 1;
-	public float timeToLevelRatio;
+	public float timeToLevelRatio = 2f;
 	public float bulletLifeTimes = 1f;
 	public float bulletSpeeds = 1f;
 	public float shotCooldown = 1f;
 	public float bulletScale = 1f;
 	public Vector2 bulletShootOffset;
 
-	public virtual void ModifyAndShoot(float playerLife, SpaceGun originGun, Color bColor){}
+	public virtual void ModifyAndShoot(float playerLife, SpaceGun originGun, Color bColor){
+
+		currentLevel = Mathf.RoundToInt(playerLife / timeToLevelRatio);
+		if (currentLevel < 1) currentLevel = 1;
+		if (maxUpgradeLevel != 0 && currentLevel > maxUpgradeLevel) currentLevel = maxUpgradeLevel;
+	}
 }
 
