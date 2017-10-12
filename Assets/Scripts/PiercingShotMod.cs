38d1a04f0c5ccc4e60e3351216469050822b02c9
Gaberino <gabriel.m.rush@gmail.com>
2017-10-12 03:22:39 -0400

    Added piercing modifier, upgrade object, and boolet hit particles

---
+++ Assets/Scripts/PiercingShotMod.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "ShotModifiers/PiercingShotMod")]
+public class PiercingShotMod : ShotModifier {
+
+	public float perLevelSizeBonus = 1;
+	public float perLevelCooldownReduction = 1;
+	public float perLevelSpeedBonus = 1f;
+
+	public override void ModifyAndShoot (float playerLife, SpaceGun originGun, Color bColor)
+	{
+		currentLevel = Mathf.RoundToInt(playerLife / timeToLevelRatio);
+		if (currentLevel < 1) currentLevel = 1;
+		if (maxUpgradeLevel != 0 && currentLevel > maxUpgradeLevel) currentLevel = maxUpgradeLevel;
+
+		float cooldownToSet = shotCooldown - (perLevelCooldownReduction * currentLevel);
+		float scaleToSet = bulletScale + (perLevelSizeBonus * currentLevel);
+
+		originGun.ShootBullet(bulletShootOffset, originGun.transform.up * (bulletSpeeds + (currentLevel * perLevelSizeBonus)), bColor, bulletLifeTimes, cooldownToSet, scaleToSet, true);
+	}
+}
