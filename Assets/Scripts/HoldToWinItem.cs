32eae74a0686ceaf606b041c405e179f11c801c5
Gaberino <gabriel.m.rush@gmail.com>
2017-10-17 10:31:18 -0400

    Current work on winstuffs (unifinished)

---
+++ Assets/Scripts/HoldToWinItem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldToWinItem : MonoBehaviour {
+
+	public Transform currentHolderTransform;
+	public float lerpSpeed;
+	private Vector3 gameStartPos;
+
+	int currentHolderID = 0;
+
+	// Use this for initialization
+	void Start () {
+		gameStartPos = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		if (col.tag.Contains ("Player")) {
+			if (currentHolderTransform == null) {
+
+				currentHolderTransform = col.transform;
+				currentHolderID = col.gameObject.GetComponent<PlayerMovementInControl> ().playerNumber; //may need to change the script being grabbed here
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/WinManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinManager : MonoBehaviour {
+
+	public static WinManager instance;
+
+	public int numOfKillsToWin;
+	public int p1Kills;
+	public int p2Kills;
+
+	public float timeObjHeldToWin;
+	public float p1HoldTime;
+	public float p2HoldTime;
+	public float timeInZoneToWin;
+	public float p1StayTime;
+	public float p2StayTIme;
+
+	// Use this for initialization
+	void Start () {
+		instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (p1HoldTime >= timeObjHeldToWin || p2HoldTime >= timeObjHeldToWin) {
+			//win via hold
+		} else if (p1Kills >= numOfKillsToWin || p2Kills >= numOfKillsToWin) {
+			//win via kills
+		} else if (p1StayTime >= timeInZoneToWin || p2StayTIme >= timeInZoneToWin) {
+			//win via king of the hill
+		}
+	}
+
+	void DisplayWinmessage(int playerInt, string method){
+		Debug.Log("Player " + playerInt + " has won via " + method); 
+	}
+}
