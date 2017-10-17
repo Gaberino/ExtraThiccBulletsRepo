618529dfb8848fc2c21588b20fef300e1f835210
Unknown <gw805@nyu.edu>
2017-10-17 13:48:19 -0400

    Weapons, Dashes, Respawns Oh My
    Added weapon swapping, dashing. fixed respawn. Added basic weapon lootcrates.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,20 +6,26 @@ using UnityEngine.UI;
 public class PlayerMovement : MonoBehaviour {
 
     public float speed;
+    public float normalSpeed;
+    /*
     public string inputControllerHorizontal;
     public string inputControllerVertical;
     public string inputControllerHorizontalLook;
     public string inputControllerVerticalLook;
     public string inputControllerFire;
+    */
     public int playerNumber;
-    
+
+    [SerializeField] private PlayerInput myInput;
+
 	public bool shootHeld = false;
-    public bool strafeControls;
+    public bool strafeControls = false;
 
     public float timeAlive;
     public int killCount;
     public int currentLifeKillCount;
     public float respawnTime;
+    public float iframes;
     public Text myScore;
 
     Rigidbody2D rbody;
@@ -30,60 +36,107 @@ public class PlayerMovement : MonoBehaviour {
     public Transform explosionPrefab;
 
 	private SpaceGun myPlayerGun;
+    [SerializeField] private ShotModifier weapon1;
+    public float weapExp1;
+    [SerializeField] private ShotModifier weapon2;
+    public float weapExp2;
+    bool equip;
+
+    public float dashTime;
+    int powerMod = 1;
+
+    Coroutine dashRoutine;
+    Coroutine iframesRoutine;
+
 	private SpriteRenderer mySR;
 	// Use this for initialization
 	void Start () {
+        myInput = this.GetComponent<PlayerInput>();
 		myPlayerGun = this.GetComponent<SpaceGun>();
 		mySR = this.GetComponent<SpriteRenderer>();
         rbody = GetComponent<Rigidbody2D>();
         dead = false;
         timeAlive = 0;
+        weapExp1 = 0;
+        weapExp2 = 0;
+        myPlayerGun.currentShotMod = weapon1;
+        equip = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float shooting = Input.GetAxisRaw(inputControllerFire);
+		// float shooting = Input.GetAxisRaw(inputControllerFire);
         if (!dead)
         {
 			timeAlive += Time.deltaTime * (currentLifeKillCount + 1);
+            if (equip) { weapExp1 += Time.deltaTime * (currentLifeKillCount + 1); }
+            else { weapExp2 += Time.deltaTime * (currentLifeKillCount + 1); }
+            processMovement();
 
-            if (strafeControls)
+            processShooting();
+
+            if (myInput.weaponSwapButtonPressed)
             {
-                shootHeld = (shooting != 0);
-                processMovement();
+                if (equip) { myPlayerGun.currentShotMod = weapon2; }
+                else { myPlayerGun.currentShotMod = weapon1; }
+                equip = !equip;
             }
-            else
-            {
-                processMovementDualStick();
-            }
-            if (shootHeld) myPlayerGun.currentShotMod.ModifyAndShoot(timeAlive, myPlayerGun, mySR.color);
         }
 	}
+
+    void processShooting()
+    {
+        if (myInput.shootButtonHeld)
+        {
+            if (equip) { powerMod = Mathf.RoundToInt(weapExp1 / myPlayerGun.currentShotMod.timeToLevelRatio); }
+            else { powerMod = Mathf.RoundToInt(weapExp2 / myPlayerGun.currentShotMod.timeToLevelRatio); }
+            if (powerMod < 1) { powerMod = 1; }
+            speed = normalSpeed / powerMod;
+            if (equip) { myPlayerGun.currentShotMod.ModifyAndShoot(weapExp1, myPlayerGun, mySR.color); }
+            else { myPlayerGun.currentShotMod.ModifyAndShoot(weapExp2, myPlayerGun, mySR.color); }
+        }
+        else
+        {
+            speed = normalSpeed;
+            powerMod = 1;
+        }
+    }
 
     void processMovement()
     {
         rbody.velocity = Vector2.zero;
-        float horizontal = Input.GetAxisRaw(inputControllerHorizontal);
-        float vertical = Input.GetAxisRaw(inputControllerVertical);
+        float horizontal = myInput.leftStickInput.x;
+        float vertical = myInput.leftStickInput.y;
+        float horizontalLook = myInput.rightStickInput.x;
+        float verticalLook = myInput.rightStickInput.y;
 
         Vector3 tempMoveDir = new Vector3(horizontal, vertical, 0);
+        Vector3 tempLookDir = new Vector3(horizontalLook, verticalLook, 0f);
 
-        if (shootHeld)
+        float currSpeed = speed;
+
+        if(dashRoutine == null)
         {
-            if (moveDir == Vector3.zero) { moveDir = tempMoveDir; }
-            transform.rotation = Quaternion.LookRotation(Vector3.forward, tempMoveDir);
+            if (tempLookDir.magnitude > 0)
+            {
+                // if (moveDir == Vector3.zero) { moveDir = tempMoveDir; }if(tempLookDir.x != 0 || tempLookDir.y != 0) 
+                { transform.rotation = Quaternion.LookRotation(Vector3.forward, tempLookDir); }
+                rbody.MovePosition(transform.position + tempMoveDir * speed * Time.deltaTime);
+            }
+            else
+            {
+                if (horizontal != 0 || vertical != 0) { lookDir = tempMoveDir; }
+                moveDir = tempMoveDir;
+                transform.rotation = Quaternion.LookRotation(Vector3.forward, lookDir);
+                rbody.MovePosition(transform.position + tempMoveDir * speed * Time.deltaTime);
+            }
+            if (myInput.dashButtonPressed) { dashRoutine = StartCoroutine(dash()); }
         }
-        else
-        {
-            if(horizontal != 0 || vertical != 0) { lookDir = tempMoveDir; }
-            moveDir = tempMoveDir;
-            transform.rotation = Quaternion.LookRotation(Vector3.forward, lookDir);
-        }
-        rbody.MovePosition(transform.position + moveDir * speed * Time.deltaTime);
     }
 
     void processMovementDualStick()
     {
+        /*
         rbody.velocity = Vector2.zero;
         float horizontal = Input.GetAxisRaw(inputControllerHorizontal);
         float vertical = Input.GetAxisRaw(inputControllerVertical);
@@ -102,18 +155,22 @@ public class PlayerMovement : MonoBehaviour {
         }
         
         rbody.MovePosition(transform.position + tempFlyDir * speed * Time.deltaTime);
+        */
     }
 
     public void Die(int killerID)
     {
-        if(killerID > 0 && killerID <= GameManager.Instance.players.Count)
+        if(iframesRoutine == null)
         {
-            PlayerMovement myKiller = GameManager.Instance.players[killerID - 1];
-            myKiller.AddScore();
+            if (killerID > 0 && killerID <= GameManager.Instance.players.Count)
+            {
+                PlayerMovement myKiller = GameManager.Instance.players[killerID - 1];
+                myKiller.AddScore();
+            }
+            currentLifeKillCount = 0;
+            timeAlive = 0;
+            StartCoroutine(respawn());
         }
-        currentLifeKillCount = 0;
-        timeAlive = 0;
-        StartCoroutine(respawn());
     }
 
     IEnumerator respawn()
@@ -122,10 +179,40 @@ public class PlayerMovement : MonoBehaviour {
         Transform newExp = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
         Destroy(newExp.gameObject, 1f);
         // Deactivate collider(s) and spriterenderer(s)
+        GetComponent<Collider2D>().enabled = false;
+        mySR.enabled = false;
         yield return new WaitForSeconds(respawnTime);
         // reactivate collider(s) and spriterenderer(s)
         dead = false;
-        transform.position = GameManager.Instance.playerSpawns[playerNumber - 1].position;
+        transform.position = GameManager.Instance.getNewSpawnLoc(this);
+        GetComponent<Collider2D>().enabled = true;
+        mySR.enabled = true;
+        iframesRoutine = StartCoroutine(freshSpawn());
+    }
+
+    public IEnumerator freshSpawn()
+    {
+        float startTime = Time.time;
+        int frameCount = 0;
+        while(Time.time - startTime < iframes)
+        {
+            if(frameCount%3 == 0) { mySR.enabled = !mySR.enabled; }
+            frameCount++;
+            yield return new WaitForEndOfFrame();
+        }
+        mySR.enabled = true;
+        iframesRoutine = null;
+    }
+
+    IEnumerator dash()
+    {
+        float startTime = Time.time;
+        while(Time.time - startTime < dashTime)
+        {
+            rbody.velocity = lookDir * (speed * 2 + 0.1f * powerMod);
+            yield return new WaitForEndOfFrame();
+        }
+        dashRoutine = null;
     }
 
     public void AddScore()
@@ -133,6 +220,19 @@ public class PlayerMovement : MonoBehaviour {
         killCount++;
         currentLifeKillCount++;
 		myScore.text = "Lv." + myPlayerGun.currentShotMod.currentLevel + " Score: " + killCount.ToString();
+    }
+
+    public void pickUpWeapon(ShotModifier newShotMod)
+    { // Replace currently held weapon and reset experience points
+        if (equip) {
+            weapon1 = newShotMod;
+            weapExp1 = 0;
+        }
+        else {
+            weapon2 = newShotMod;
+            weapExp2 = 0;
+        }
+        myPlayerGun.currentShotMod = newShotMod;
     }
 
 //    void OnCollisionEnter2D(Collision2D coll)
---
+++ Assets/Scripts/WeaponBax.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponBax : MonoBehaviour {
+
+    public ShotModifier weaponHeld;
+    public float lifeTime;
+    float startTime;
+
+	// Use this for initialization
+	void Start () {
+        startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time - startTime >= lifeTime)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        PlayerMovement player = coll.GetComponent<PlayerMovement>();
+        if(player != null)
+        {
+            player.pickUpWeapon(weaponHeld);
+            Destroy(gameObject);
+        }
+    }
+}
