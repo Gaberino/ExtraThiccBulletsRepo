8713f4c0756db04fac1516652765d3c3d5c5404b
Unknown <tgr235@nyu.edu>
2017-11-14 15:01:03 -0500

    update turner playtest

--- Assets/Scripts/subTeleMod.cs
+++ Assets/Scripts/subTeleMod.cs
@@ -2,22 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-[CreateAssetMenu(menuName = "SubModifiers/subShieldMod")]
-public class subTeleMod : MonoBehaviour {
-	public GameObject teleport_loc;
-	private Vector3 location;
-
-
-	public override void runSubAction (PlayerMovement xXx_pla_Move_xXx)
-	{
-		teleport_loc.
-		if (){}
-		location = teleport_loc.transform.position;
-
-
-		this.transform.position = location;
-
-
-	}
-}
+//
+//[CreateAssetMenu(menuName = "SubModifiers/subShieldMod")]
+//public class subTeleMod : MonoBehaviour {
+//	public GameObject teleport_loc;
+//	private Vector3 location;
+//
+//
+//	public override void runSubAction (PlayerMovement xXx_pla_Move_xXx)
+//	{
+//		teleport_loc.
+//		if (){}
+//		location = teleport_loc.transform.position;
+//
+//
+//		this.transform.position = location;
+//
+//
+//	}
+//}
