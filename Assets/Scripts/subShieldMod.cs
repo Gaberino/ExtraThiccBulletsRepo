42e16651d092f27a5da689c74b3d723a368559ab
Unknown <gw805@nyu.edu>
2017-11-16 15:18:03 -0500

    shield parent. they're huge. that's all

--- Assets/Scripts/subShieldMod.cs
+++ Assets/Scripts/subShieldMod.cs
@@ -9,8 +9,8 @@ public class subShieldMod : SubModifier {
 
 	public override void runSubAction (PlayerMovement xXx_pla_Move_xXx)
 	{
-		Instantiate (sinwaveshield, xXx_pla_Move_xXx.transform.position, Quaternion.identity);
-
+		GameObject shield = Instantiate (sinwaveshield, xXx_pla_Move_xXx.transform.position, Quaternion.identity);
+        shield.transform.parent = xXx_pla_Move_xXx.transform;
 	}
 
 }
